339696a5ae37566a7d95864106456c60eaff650c
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-07-09 04:10:37 +0700

    upload lan dau

---
+++ day9-sound-broad-project/script.js
@@ -0,0 +1,27 @@
+const sounds = [
+  "aritis-galaxy",
+  "blue-ballad",
+  "epic-cinema",
+  "fairy-tale",
+  "successful",
+  "town",
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
